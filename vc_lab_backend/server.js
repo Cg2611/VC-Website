7a20a0b229df0f0a6545571aa20660ecf827cd7d
Chaithanya Ganesh <96193964+Cg2611@users.noreply.github.com>
2024-07-26 03:15:22 +0530

    Done with all db data entry

--- vc_lab_backend/server.js
+++ vc_lab_backend/server.js
@@ -49,6 +49,67 @@ app.post('/submit-internship', upload.none(), (req, res) => {
     });
 });
 
+// Route to handle training form submission
+app.post('/submit-training', upload.none(), (req, res) => {
+    const { name, email, phone, program, payment, comments } = req.body;
+
+    if (!name || !email || !phone || !program || !payment) {
+        return res.status(400).send('Missing required fields');
+    }
+
+    const sql = 'INSERT INTO TrainingForm (name, email, phone_number, program_of_interest, paymentdrivelink, comments) VALUES (?, ?, ?, ?, ?, ?)';
+    db.query(sql, [name, email, phone, program, payment, comments], (err, result) => {
+        if (err) {
+            console.error('Database error:', err);
+            return res.status(500).send('An error occurred while submitting the form.');
+        }
+        res.send('Training registration submitted successfully');
+    });
+});
+
+
+app.post('/submit-team-application', upload.none(), (req, res) => {
+    const { name, email, phone, specialization, linkedin, scopus, resumeLink } = req.body;
+
+    // Ensure that all required fields are present
+    if (!name || !email || !phone || !specialization || !linkedin || !resumeLink) {
+        return res.status(400).send('Missing required fields');
+    }
+
+    const sql = 'INSERT INTO JoinForm (name, email, phone_number, specialization, resumedrivelink, linkedinprofilelink, scopusprofilelink) VALUES (?, ?, ?, ?, ?, ?, ?)';
+    db.query(sql, [name, email, phone, specialization, resumeLink, linkedin, scopus], (err, result) => {
+        if (err) {
+            console.error('Database error:', err);
+            return res.status(500).send('An error occurred while submitting the form.');
+        }
+        res.send('Application to join VC team submitted successfully');
+    });
+});
+
+app.post('/submit-contact-form', upload.none(), (req, res) => {
+    const { name, email, message } = req.body;
+
+    // Ensure that all required fields are present
+    if (!name || !email || !message) {
+        return res.status(400).send('All fields are required');
+    }
+
+    // SQL query to insert the data into the contact_us table
+    const sql = 'INSERT INTO contact_us (name, email, message) VALUES (?, ?, ?)';
+    db.query(sql, [name, email, message], (err, result) => {
+        if (err) {
+            console.error('Database error:', err);
+            return res.status(500).send('An error occurred while submitting the form.');
+        }
+        res.send('Message sent successfully');
+    });
+});
+
+
+
+
+
+
 // Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
